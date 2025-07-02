1521b4c5ffa5b93529f61e8525c4e9e86f12a040
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-07-02 18:12:44 +0900

    refactor(responsebody)! : 리스폰스 바디 어노테이션
    ### 변경점
    - 컨트롤러 어노테이션을 @RestController에서 @Controller로 변경
    - 관리자 전용 기능의 리턴 타입을 String으로 변경, 반환 내용도 "redirect"로 변경
    - 그 이외의 공용/유저용 기능은 @ResponseBody 어노테이션 부착

--- src/main/java/sesac/bookmanager/question/QuestionController.java
+++ src/main/java/sesac/bookmanager/question/QuestionController.java
@@ -3,36 +3,38 @@ package sesac.bookmanager.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import sesac.bookmanager.question.data.*;
 import sesac.bookmanager.security.CustomUserDetails;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
+    @ResponseBody
     @PostMapping("/create")
     public ResponseEntity<QuestionResponse> createQuestion(@RequestBody QuestionCreateRequest request,
                                                            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         return ResponseEntity.ok(questionService.createQuestion(request, customUserDetails));
     }
 
-
+    @ResponseBody
     @GetMapping
     public ResponseEntity<QuestionPageResponse> getAllQuestions(QuestionSearchRequest search) {
         return ResponseEntity.ok(questionService.getAllQuestions(search));
     }
 
-
+    @ResponseBody
     @GetMapping("/{questionId}")
     public ResponseEntity<QuestionWithReplyResponse> getQuestionById(@PathVariable Integer questionId) {
         return ResponseEntity.ok(questionService.getQuestionById(questionId));
     }
 
-
+    @ResponseBody
     @PutMapping("/{questionId}/edit")
     public ResponseEntity<QuestionResponse> updateQuestion(@PathVariable Integer questionId, @RequestBody QuestionUpdateRequest request,
                                                            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
@@ -40,16 +42,17 @@ public class QuestionController {
     }
 
     @PutMapping("/{questionId}/progress")
-    public ResponseEntity<QuestionResponse> progressEdit(@PathVariable Integer questionId, @ModelAttribute QuestionStatusUpdateRequest request) {
-        return ResponseEntity.ok(questionService.updateStatus(questionId, request));
+    public String progressEdit(@PathVariable Integer questionId, @ModelAttribute QuestionStatusUpdateRequest request) {
+
+        questionService.updateStatus(questionId, request);
+        return "redirect:/api/question/" + questionId;
     }
 
-
     @DeleteMapping("/{questionId}")
-    public ResponseEntity<Void> deleteQuestion(@PathVariable Integer questionId,
+    public String deleteQuestion(@PathVariable Integer questionId,
                                                @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         questionService.deleteQuestion(questionId, customUserDetails);
 
-        return ResponseEntity.noContent().build();
+        return "redirect:/api/question";
     }
 }
