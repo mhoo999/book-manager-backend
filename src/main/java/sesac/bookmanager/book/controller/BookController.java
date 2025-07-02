720649b0f7b5f8ac3d4b437967f62e4ce984f997
myeonghoon <mhoo999@naver.com>
2025-07-02 12:02:39 +0900

    book branch  강제 병합
    Signed-off-by: latte3cup <latte3cup@naver.com>

---
+++ src/main/java/sesac/bookmanager/book/controller/BookController.java
@@ -0,0 +1,43 @@
+package sesac.bookmanager.book.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sesac.bookmanager.book.dto.request.CreateBookRequestDto;
+import sesac.bookmanager.book.dto.request.SearchBookRequestDto;
+import sesac.bookmanager.book.dto.request.UpdateBookRequestDto;
+import sesac.bookmanager.book.dto.response.BookIdResponseDto;
+import sesac.bookmanager.book.dto.response.BookResponseDto;
+import sesac.bookmanager.book.dto.response.PageBookResponseDto;
+import sesac.bookmanager.book.service.BookService;
+
+@RestController
+@RequestMapping("/api/v1/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping("/register")
+    public ResponseEntity<BookIdResponseDto> createBook(@Valid @RequestBody CreateBookRequestDto request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.createBook(request));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<PageBookResponseDto> searchBooks(SearchBookRequestDto request) {
+        return ResponseEntity.ok(bookService.searchBooks(request));
+    }
+
+    @GetMapping("/{bookId}")
+    public ResponseEntity<BookResponseDto> getBook(@PathVariable long bookId) {
+        return ResponseEntity.ok(bookService.getBook(bookId));
+    }
+
+    @PutMapping("/{bookId}")
+    public ResponseEntity<BookIdResponseDto> updateBook(@PathVariable Long bookId, @RequestBody UpdateBookRequestDto request) {
+        return ResponseEntity.ok(bookService.updateBook(bookId, request));
+    }
+
+}
