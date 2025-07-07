fe645bc4a40dd4d7851d84fad87a5839447f42b7
hoon <mhoo999@naver.com>
2025-07-07 15:54:52 +0900

    feat (book-cover) : 북 커버 이미지 저장 기능
    - webconfig 수정

--- src/main/java/sesac/bookmanager/global/config/WebConfig.java
+++ src/main/java/sesac/bookmanager/global/config/WebConfig.java
@@ -2,7 +2,6 @@ package sesac.bookmanager.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:src/main/resources/static/images/")
+                .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
+
+        // images 패턴 추가
+        registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:src/main/resources/static/images/")
                 .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
     }
