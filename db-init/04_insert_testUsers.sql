a2814c24d75d78a50f15c992e197656ae0e6747e
latte3cup <latte3cup@naver.com>
2025-07-07 16:38:44 +0900

    04_insert_testUsers 변경 (유저 3명으로 함으로써 가데이터 제약 해결)
    Signed-off-by: latte3cup <latte3cup@naver.com>

--- db-init/04_insert_testUsers.sql
+++ db-init/04_insert_testUsers.sql
@@ -1,4 +1,5 @@
 INSERT INTO user (
     email, name, phone, pwd, is_deleted, created_at
-) VALUES ('example@email.com1','홍길동','010-1234-5678','$2a$12$R6Uu24.vh/XB2ASdxXbWsun6uBYXBNrNUQtGc9S6wn91o/uzaXPDq',0,NOW()), --hashed_password
-         ('example@email.com2','홍길동2','010-1234-5678','$2a$12$R6Uu24.vh/XB2ASdxXbWsun6uBYXBNrNUQtGc9S6wn91o/uzaXPDq',0,NOW()) --user1234
+) VALUES ('example@email.com1','홍길동','010-1234-5678','$2a$12$R6Uu24.vh/XB2ASdxXbWsun6uBYXBNrNUQtGc9S6wn91o/uzaXPDq',0,NOW()),  /* hashed_password */
+         ('example@email.com2','홍길동2','010-1234-5628','$2a$10$nLjPphIzVRvsxtcPy5BYmeKxVrlmKDjeLZwQtgeazw6FKRj.kv4Uy',0,NOW()),/* user1234 */
+         ('example@email.com3','홍길동3','010-1234-5278','$2a$10$nLjPphIzVRvsxtcPy5BYmeKxVrlmKDjeLZwQtgeazw6FKRj.kv4Uy',0,NOW());  /* user1234 */
