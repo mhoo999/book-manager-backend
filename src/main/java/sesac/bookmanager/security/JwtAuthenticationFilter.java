48a6ab6599cc7dd5ad039a53c11138e0c15a9ba2
latte3cup <latte3cup@naver.com>
2025-07-08 11:07:59 +0900

    jwt인증 필터에 대해 인증없이 개방한 페이지는 jwt 검증 로직을 건너뛰고 필터 종료.
    Signed-off-by: latte3cup <latte3cup@naver.com>

--- src/main/java/sesac/bookmanager/security/JwtAuthenticationFilter.java
+++ src/main/java/sesac/bookmanager/security/JwtAuthenticationFilter.java
@@ -15,12 +15,16 @@ import sesac.bookmanager.user.data.User;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final jwtTokenProvider jwtTokenProvider;
+    private static final List<String> WHITELIST = List.of(
+            "/", "/home", "/api/auth", "/api/notice", "/api/question", "/api/reply", "/api/v1/books"
+    );
 
     @Autowired
     public JwtAuthenticationFilter(jwtTokenProvider jwtTokenProvider) {
@@ -30,6 +34,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        /* 해당 경로는 토큰 없이 필터를 건너뛰도록 적용*/
+        final String path = request.getServletPath();
+        boolean isWhitelisted = WHITELIST.stream().anyMatch(path::startsWith);
+
+        if (isWhitelisted) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         final String authHeader = request.getHeader("Authorization");
         final String jwtToken;
@@ -40,8 +52,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
 
-        jwtToken = authHeader.substring(7);
-        claims = jwtTokenProvider.getAllClaimsFromToken(jwtToken);
+        /* 유효하지 않은 토큰은 401 에러 응답*/
+        try {
+            jwtToken = authHeader.substring(7);
+            claims = jwtTokenProvider.getAllClaimsFromToken(jwtToken);
+        } catch (Exception ex) {
+            // 유효하지 않은 토큰이면 그냥 인증 없이 진행하거나
+            // 혹은 401 에러로 응답
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid JWT Token");
+            return;
+        }
+
         final int userId = Integer.parseInt(claims.getSubject());
         final String userEmail = claims.get("email", String.class);
         final String userName = claims.get("name",String.class);
