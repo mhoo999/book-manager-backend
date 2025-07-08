d9ff359ba7cb653d4001a6cf34c37a89336f6d7e
myeonghoon <mhoo999@naver.com>
2025-07-08 12:06:50 +0900

    fix(rent) : book, rent 가데이터 변경

--- db-init/10_insert_rent_history.sql
+++ db-init/10_insert_rent_history.sql
@@ -1,9 +1,7 @@
 ﻿INSERT INTO book_manager.rent_history (user_id,book_item_id,admin_id,rental_date,expected_return_date,status,description) VALUES
-                                                                                                                              (2,1,NULL,'2025-07-07 19:17:32',NULL,'REQUESTED',NULL),
-                                                                                                                              (2,3,NULL,'2025-07-07 19:17:38',NULL,'REQUESTED',NULL),
-                                                                                                                              (2,7,NULL,'2025-07-07 19:17:47',NULL,'REQUESTED',NULL),
-                                                                                                                              (2,9,NULL,'2025-07-07 19:17:51',NULL,'REQUESTED',NULL),
-                                                                                                                              (2,11,NULL,'2025-07-07 19:17:55',NULL,'REQUESTED',NULL),
-                                                                                                                              (2,13,NULL,'2025-07-07 19:17:58',NULL,'REQUESTED',NULL),
-                                                                                                                              (2,16,NULL,'2025-07-07 19:18:02',NULL,'REQUESTED',NULL),
-                                                                                                                              (2,18,NULL,'2025-07-07 19:18:05',NULL,'REQUESTED',NULL);
+                                                                                                                              (2,1,1,'2025-07-08 12:00:41','2025-07-08','RETURNED','3회 미납으로 3개월간 대여 금지'),
+                                                                                                                              (2,6,1,'2025-07-08 12:01:09','2025-07-22','OVERDUE',NULL),
+                                                                                                                              (2,13,1,'2025-07-08 12:01:33','2025-07-22','RENTED',NULL),
+                                                                                                                              (3,7,1,'2025-07-08 12:01:40','2025-07-22','RENTED',NULL),
+                                                                                                                              (3,14,1,'2025-07-08 12:00:16',NULL,'REJECTED','거절'),
+                                                                                                                              (3,18,NULL,'2025-07-08 12:00:26',NULL,'REQUESTED',NULL);
