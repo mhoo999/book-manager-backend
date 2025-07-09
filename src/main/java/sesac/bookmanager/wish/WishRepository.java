f7530bab7c67f76c5265a4f8359be65ffab507a4
latte3cup <latte3cup@naver.com>
2025-07-09 11:46:31 +0900

    희망도서 신청 사항 (대시보드) 추가
    Signed-off-by: latte3cup <latte3cup@naver.com>

--- src/main/java/sesac/bookmanager/wish/WishRepository.java
+++ src/main/java/sesac/bookmanager/wish/WishRepository.java
@@ -1,14 +1,16 @@
 package sesac.bookmanager.wish;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sesac.bookmanager.wish.data.Wish;
 import sesac.bookmanager.wish.data.WishStatus;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface WishRepository extends JpaRepository<Wish, Integer> {
     List<Wish> findByStatusBetween(WishStatus status, WishStatus status2);
+
+    int countByUserId(@Param("userId") int userId);
 }
